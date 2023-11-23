89861fd7137e1e0e9dce4620bd36c7e2fe5b4971
Iyaiyak <126131186+Iyaiyak@users.noreply.github.com>
2023-11-23 10:11:21 +0700

    ubahan dikit

--- coba.java
+++ coba.java
@@ -1,6 +1,42 @@
+abstract class Manusia{
+    String Nama;
+    String Alamat;
+    int Age;
+
+    public Manusia(String Nama, String Alamat, int Age){
+        this.Nama = Nama;
+        this.Alamat = Alamat;
+        this.Age = Age;
+    }
+
+}
+
+class gamer extends Manusia{
+    String Rank;
+    gamer(String Nama, String Alamat, int Age, String Rank){
+        super(Nama, Alamat, Age);
+        this.Rank = Rank;
+    }
+
+    void display(){
+        String hasil;
+        System.out.println("Nama    : "+Nama);
+        System.out.println("Alamat  : " +Alamat);
+        System.out.println("Umur    : " +Age);
+        System.out.println("Rank    : " +Rank);
+        if(Rank == "bronze"){
+            hasil = "CUPU";
+        } else{
+            hasil = "JAGO";
+        }
+        System.out.println("Status  : " +hasil);
+    }
+
+}
+
 public class coba{
     public static void main(String[] args){
-        System.out.println("Odit Pro Player Dota 2");
-
+    gamer data = new gamer("Liu", "Malang", 99, "bronze");
+    data.display();
     }
 }

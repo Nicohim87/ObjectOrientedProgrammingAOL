a57dc5b808be136280db7bd730d806255e3e5bfc
Liuboyyy <stevenliu470@gmail.com>
2023-11-23 09:54:48 +0700

    Update coba.java

--- coba.java
+++ coba.java
@@ -1,6 +1,6 @@
 public class coba{
     public static void main(String[] args){
-        System.out.println("Odit Pro Player Dota 2");
+        System.out.println("Odit Pro Player Dota 2 tapi bo'ong");
 
     }
 }

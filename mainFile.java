5405af193692f50d01914133cd67f3e882cb31b4
Nicohim87 <nico.himawan@binus.ac.id>
2023-11-27 16:00:33 +0700

    Adding Class

--- mainFile.java
+++ mainFile.java
@@ -1,6 +1,142 @@
 import java.util.Scanner;
 import javax.swing.*;
 
+class Barang{
+    private String ID;
+    private String nama;
+    private String jenis;
+    private int jumlah;
+    private double harga;
+    private String keterangan;
+
+    void setElement(String ID, String nama, String jenis, int jumlah, double harga, String keterangan){
+        this.ID = ID;
+        this.nama = nama;
+        this.jenis = jenis;
+        this.jumlah = jumlah;
+        this.harga = harga;
+        this.keterangan = keterangan;
+    }
+
+    String getID(){
+        return this.ID;
+    }
+
+    void setNama(String nama){
+        this.nama = nama;
+    }
+    String getNama(){
+        return this.nama;
+    }
+
+    void setJenis(String jenis){
+        this.jenis = jenis;
+    }
+    String getJenis(){
+        return this.jenis;
+    }
+
+    void setJumlah(int jumlah){
+        this.jumlah = jumlah;
+    }
+    int getJumlah(){
+        return this.jumlah;
+    }
+    void increaseJumlah(int amt){
+        this.jumlah += amt;
+    }
+    void reduceJumlah(int amt){
+        this.jumlah -= amt;
+    }
+
+    void setHarga(double harga){
+        this.harga = harga;
+    }
+    double getHarga(){
+        return this.harga;
+    }
+
+    void setKeterangan(String keterangan){
+        this.keterangan = keterangan;
+    }
+    String getKeterangan(){
+        return this.keterangan;
+    }
+
+}
+
+class detailTransaksi{
+    private String transaksiID;
+    private String barangID;
+    private int jumlah;
+
+    String getTransaksiID() {
+        return transaksiID;
+    }
+    void setTransaksiID(String transaksiID) {
+        this.transaksiID = transaksiID;
+    }
+
+    String getBarangID() {
+        return barangID;
+    }
+    void setBarangID(String barangID) {
+        this.barangID = barangID;
+    }
+
+    int getJumlah() {
+        return jumlah;
+    }
+    void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+}
+
+class Tanggal{
+    private int tahun;
+    private int bulan;
+    private int hari;
+    private int jam;
+    private int menit;
+
+    void setTanggal(int tahun, int bulan, int hari){
+        this.tahun = tahun;
+        this.bulan = bulan;
+        this.hari = hari;
+    }
+
+    void printTanggal(){
+        System.out.printf("%d/%d/%d, %d:%d", this.hari, this.bulan, this.tahun, this.jam, this.menit);
+    }
+}
+
+class HeaderTransaksi{
+    private String transaksiID;
+    private String nama;
+    private String staff;
+    Tanggal tanggalTransaksi;
+
+    String getTransaksiID() {
+        return transaksiID;
+    }
+    void setTransaksiID(String transaksiID) {
+        this.transaksiID = transaksiID;
+    }
+
+    String getNama() {
+        return nama;
+    }
+    void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    String getStaff() {
+        return staff;
+    }
+    void setStaff(String staff) {
+        this.staff = staff;
+    }
+}
 
 public class mainFile{
     public static void main(String[] args){
